4109d45b3d410685744cf2e9b16912b5774809cc
dailybp <bpdailey@gmail.com>
2017-11-03 23:22:03 -0400

    first draft

---
+++ bamazon.sql
@@ -0,0 +1,16 @@
+--Drops the bamazon database if it already exists--
+DROP DATABASE IF EXISTS bamazon;
+--creates a new bamazon database--
+CREATE DATABASE bamazon;
+--Makes all of the following code affect the bamazon database--
+USE bamazon;
+
+--creates the table products in the bamazon database--
+CREATE TABLE products(
+	item_id INT NOT NULL AUTO_INCREMENT,
+	product_name VARCHAR(50) NOT NULL,
+	department_name VARCHAR(50) NOT NULL,
+	price DECIMAL (10,2),
+	stock_quantity INT DEFAULT 0,
+	PRIMARY KEY (item_id)
+);
